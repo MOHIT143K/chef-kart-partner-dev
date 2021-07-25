2307bec2c32707004f45616246259a54cd030645
Nikhil-Pers <nikhil051097@gmail.com>
2021-07-25 13:09:53 +0530

    Updated error to message

--- routes/user/delete-back-account.js
+++ routes/user/delete-back-account.js
@@ -17,7 +17,7 @@ export const deleteBankAccount = async (req, res) => {
       return res.status(404).json({error: "Not Found"});
     }
 
-    return res.status(200).json({error: "Deleted Successfully!"});
+    return res.status(200).json({message: "Deleted Successfully!"});
   } catch (error) {
     console.log(error);
     return res.status(500).json({error: "Server Error!"});
